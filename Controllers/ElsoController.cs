b0ead87d436e8beca39b7e340b12dd27e35fe782
mocsa <mocsa@LAPTOP-1SGO21PF>
2024-04-29 00:14:15 +0200

    Add project files.

---
+++ Controllers/ElsoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Christoc.Modules.SajatModulUj.Controllers
+{
+    public class ElsoController : Controller
+    {
+        // GET: Elso
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/MasodikController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Christoc.Modules.SajatModulUj.Controllers
+{
+    public class MasodikController : Controller
+    {
+        // GET: Masodik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
